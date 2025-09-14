8a41b1928756260ca4a198abc39fdf0d6ce8454a
SeyedMojtaba1 <seyedmojtabamirsoleimani@gmail.com>
2025-09-14 05:04:12 +0330

    Nginx Task

---
+++ Month 1/Web Server/SeyedMojtabaMirsoleimani/nginx/app2.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def handle():
+    return "Hello from Semir! From port 3001."
+
+if "__main__" == __name__:
+    app.run(port=3001)
